f7e46abae7a209af3ba079092913244652d47124
1binfo <1binfo@DESKTOP-K3LBSU3.basilides.local>
2019-03-15 20:42:48 -0300

    criação do metodo de soma

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,38 @@
+import javax.swing.JOptionPane;
+
+public class Calculadora {
+
+	private int valor1;
+	private int valor2;
+	public Calculadora() {
+
+	}
+
+	public Calculadora(int valor1, int valor2) {
+		this.valor1 = valor1;
+		this.valor2 = valor2;
+	}
+
+	public int getValor1() {
+		return valor1;
+	}
+
+	public void setValor1(int valor1) {
+		this.valor1 = valor1;
+	}
+
+	public int getValor2() {
+		return valor2;
+	}
+
+	public void setValor2(int valor2) {
+		this.valor2 = valor2;
+	}
+
+	public void somar() {
+		valor1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor1"));
+		valor2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor2"));
+		System.out.println("a soma é" + (valor1+valor2));
+	}
+
+}
